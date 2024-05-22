00ec5c0358324f2a47c8aa22a48c4a411e937978
malakehab2003 <lokaehab2003@gmail.com>
2024-05-22 11:53:42 +0300

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,9 +8,9 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ get and put data in a dictionary """
     def put(self, key, item):
-        """ use self.cache_data - dictionary from the parent class BaseCaching """
+        """ use self.cache_data from the parent class BaseCaching """
         if key is not None and item is not None:
-           self. cache_data[key] = item
+            self. cache_data[key] = item
 
     def get(self, key):
         """ get data from self.cache_data - dictionary of specific key """
