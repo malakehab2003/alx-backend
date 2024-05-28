f5ddaa6a07aad75677a97ec3b3668145ac3e8766
malakehab2003 <lokaehab2003@gmail.com>
2024-05-28 23:35:08 +0300

    task 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Create a single / route """
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config:
