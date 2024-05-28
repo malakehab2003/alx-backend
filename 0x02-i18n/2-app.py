1c04e67bb6425b3cdfd1ea1d55bec977d8a39e13
malakehab2003 <lokaehab2003@gmail.com>
2024-05-28 23:24:20 +0300

    task 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,10 +18,10 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """ determine the best language """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', strict_slashes=False)
