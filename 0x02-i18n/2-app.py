dc679ae5b72240583fa863e0a49150eb40909fd4
malakehab2003 <lokaehab2003@gmail.com>
2024-05-28 20:14:19 +0300

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Create a single / route """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Configuration for Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best language """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ create the / route """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
