b5aa826976742147ff60c5cc0d1a0eb4e851873c
malakehab2003 <lokaehab2003@gmail.com>
2024-06-10 01:27:21 +0300

    task 8

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+""" Create a single / route """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, format_datetime
+import pytz
+from pytz import exceptions
+
+
+class Config:
+    """ Configuration for Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best language """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    user = g.get('user')
+    if user and user['locale'] in app.config['LANGUAGES']:
+        return user['locale']
+
+    header = request.headers.get('locale')
+    if header and header in app.config['LANGUAGES']:
+        return header
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """ determine the best time zone """
+    timezone = request.args.get('timezone')
+    if timezone:
+        try:
+            pytz.timezone(timezone)
+            return timezone
+        except exceptions.UnknownTimeZoneError:
+            pass
+
+    user = g.get('user')
+    if user:
+        timezone = user.get('timezone')
+        if timezone:
+            try:
+                pytz.timezone(timezone)
+                return timezone
+            except exceptions.UnknownTimeZoneError:
+                pass
+    header_time = request.headers.get('timezone')
+    if header_time:
+        try:
+            pytz.timezone(header_time)
+            return header_time
+        except exceptions.UnknownTimeZoneError:
+            pass
+
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+def get_user():
+    """ get the logged in user """
+    user = request.args.get('login_as')
+    if user and int(user) in users.keys():
+        return users[int(user)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """ before request """
+    g.user = get_user()
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ create the / route """
+    g.time = format_datetime()
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
