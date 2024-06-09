5f555146f315138a520174b3e6e9aae8ca49e854
malakehab2003 <lokaehab2003@gmail.com>
2024-06-09 23:04:05 +0300

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,6 +21,9 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """ determine the best language """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
