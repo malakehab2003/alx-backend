82fe6d72651786fab726499290ff5ca2c9b5b074
malakehab2003 <lokaehab2003@gmail.com>
2024-06-25 23:25:07 +0300

    task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import redis from 'redis';
+
+const connect = redis.createClient();
+
+connect.on('conncet', () => {
+  console.log('Redis client connected to the server');
+});
+
+connect.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+})
